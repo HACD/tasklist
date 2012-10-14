922ec79036408631d5651425deda5ea085501d14
Anthony Sellitti <anthony.github@asellitt.com>
2012-10-14 15:41:36 +1100

    added tasks tree to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+root = Task.create(name: 'root', description: 'desc')
+
+task = Task.new(name: 'task1', description: 'desc')
+task.parent = root
+task.save
+
+task = Task.new(name: 'task2', description: 'desc')
+task.parent = root
+task.save
+
+root = task
+task = Task.new(name: 'task3', description: 'desc')
+task.parent = root
+task.save
+
+task = Task.new(name: 'task4', description: 'desc')
+task.parent = root
+task.save
+
+task = Task.new(name: 'task5', description: 'desc')
+task.parent = root
+task.save
+
+root = task
+task = Task.new(name: 'task6', description: 'desc')
+task.parent = root
+task.save
+
+root = task
+task = Task.new(name: 'task7', description: 'desc')
+task.parent = root
+task.save
+
+task = Task.new(name: 'task8', description: 'desc')
+task.parent = root
+task.save
