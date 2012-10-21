af069b7cba6f3b63f57f89642ccef9b7d9b9f4fc
Sarma <Sarma@Saturn.local>
2012-10-21 17:49:58 +1100

    changed completed to completed_at, added functionality to hide parent selector on new child node creation.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  attr_accessible :completed, :description, :name, :parent_id, :completed
+  attr_accessible :completed_at, :description, :name, :parent_id
 
   has_ancestry :orphan_strategy => :rootify
 
