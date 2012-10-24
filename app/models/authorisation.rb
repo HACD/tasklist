dfd19c7ff34fdc2fff095bcf059a2ad4166e23b7
Anthony Sellitti <anthony.github@asellitt.com>
2012-10-24 21:49:28 +1100

    fixed auth issue, added user as an accessible attr"

--- app/models/authorisation.rb
+++ app/models/authorisation.rb
@@ -1,13 +1,18 @@
 class Authorisation < ActiveRecord::Base
-  attr_accessible :provider, :uid, :user_id
+  attr_accessible :provider, :uid, :user_id, :user
 
   belongs_to :user
   validates :provider, :uid, :presence => true
 
   def self.find_or_create(auth_hash)
-    unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-      user = User.create :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
-      auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    puts '#'
+    puts '#'
+    puts auth_hash.inspect
+    puts '#'
+    puts '#'
+    unless auth = find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
+      user = User.create(name: auth_hash['info']['name'], email: auth_hash['info']['email'])
+      auth = create(user: user, provider: auth_hash['provider'], uid: auth_hash['uid'])
     end
     auth
   end
